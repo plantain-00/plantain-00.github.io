5ffc94b639a18e9732b0f73ef232286edb5dc16d
姚耀 <yaoyao12306@163.com>
2015-05-17 20:36:58 +0800

    save

--- playthings/requirejs/main.js
+++ playthings/requirejs/main.js
@@ -1,3 +1,11 @@
-require(["./math"], function (math, template) {
+require.config({
+    paths: {
+        jquery: "./jquery-2.1.4.min",
+        math: "./math"
+    }
+});
+
+require(["math", "jquery"], function (math, $) {
     console.log(math.add(1, 2));
+    $("body").html("<button>click it</button>");
 });
