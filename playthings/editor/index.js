98a30eaa36e3c82256b1b15b52cc381771983baf
yaoyao <yaoyao12306@163.com>
2015-06-01 09:00:17 +0800

    add todo

--- playthings/editor/index.js
+++ playthings/editor/index.js
@@ -3,41 +3,37 @@ require("./index.css");
 $(function () {
     hljs.initHighlightingOnLoad();
 
-    var textareas = document.getElementsByTagName('textarea');
-    var count = textareas.length;
-    for (var i = 0; i < count; i++) {
-        textareas[i].onkeydown = function (e) {
-            var keyCode = e.keyCode || e.which;
+    $("#content").keydown(function (e) {
+        var keyCode = e.keyCode || e.which;
 
-            if (keyCode == 9) {
-                e.preventDefault();
-                var start = this.selectionStart;
-                var end = this.selectionEnd;
-                var val = this.value;
-                var selected = val.substring(start, end);
-                var re, count;
+        if (keyCode == 9) {
+            e.preventDefault();
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+            var val = this.value;
+            var selected = val.substring(start, end);
+            var re, count;
 
-                if (e.shiftKey) {
-                    re = /^\t/gm;
-                    count = -selected.match(re).length;
-                    this.value = val.substring(0, start) + selected.replace(re, '') + val.substring(end);
-                    // todo: add support for shift-tabbing without a selection
-                } else {
-                    re = /^/gm;
-                    count = selected.match(re).length;
-                    this.value = val.substring(0, start) + selected.replace(re, '\t') + val.substring(end);
-                }
-
-                if (start === end) {
-                    this.selectionStart = end + count;
-                } else {
-                    this.selectionStart = start;
-                }
-
-                this.selectionEnd = end + count;
+            if (e.shiftKey) {
+                re = /^\t/gm;
+                count = -selected.match(re).length;
+                this.value = val.substring(0, start) + selected.replace(re, '') + val.substring(end);
+                // todo: add support for shift-tabbing without a selection
+            } else {
+                re = /^/gm;
+                count = selected.match(re).length;
+                this.value = val.substring(0, start) + selected.replace(re, '\t') + val.substring(end);
             }
+
+            if (start === end) {
+                this.selectionStart = end + count;
+            } else {
+                this.selectionStart = start;
+            }
+
+            this.selectionEnd = end + count;
         }
-    }
+    });
 
     var md = markdownit();
 
