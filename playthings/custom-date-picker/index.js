2c19e8832912e9b9026da72edf8231e96cfcc118
york yao <york.yao@dragonsoftbravo.com>
2015-08-07 18:16:25 +0800

    save

--- playthings/custom-date-picker/index.js
+++ playthings/custom-date-picker/index.js
@@ -1,4 +1,3 @@
-var calendarBodyTemplate = require("./templates/calendar.min.handlebars");
 var fullImageUrl = require("./images/full.png");
 var orderedImageUrl = require("./images/ordered.png");
 
@@ -46,13 +45,17 @@ var vueModel = {
                         result.push(yearMonth + "-" + (theDay < 10 ? "0" : "") + theDay);
                     }
                 }
-
-                return result;
             }
-        }
-    },
-    methods: {
-        getDates: function () {
+
+            return result;
+        },
+        canShowPreviousMonth: function () {
+            return new Date(this.year, this.month, 1) > this.minDay;
+        },
+        canShowNextMonth: function () {
+            return new Date(this.year, this.month + 1, 0) < this.maxDay;
+        },
+        weeks: function () {
             var maxDayOfMonth = new Date(this.year, this.month + 1, 0).getDate();
             var firstDayOfMonth = new Date(this.year, this.month, 1).getDay();
 
@@ -72,62 +75,88 @@ var vueModel = {
             var tmp = [];
             for (var i = 0; i < firstDayOfMonth; i++) {
                 tmp.push({
-                    isVisible: false
+                    tdClass: "",
+                    text: "",
+                    isFull: false,
+                    isOrdered: false,
+                    labelClass: ""
                 });
             }
 
             for (var j = 1; j <= maxDayOfMonth; j++) {
                 if (tmp.length == 7) {
-                    result.push(tmp);
+                    result.push({
+                        days: tmp
+                    });
                     tmp = [];
                 }
 
                 if ($.inArray(j, holidaysOfMonth) >= 0) {
                     tmp.push({
-                        isVisible: true,
+                        tdClass: "holiday",
                         text: j,
-                        isHoliday: true
+                        isFull: false,
+                        isOrdered: false,
+                        labelClass: ""
                     });
                 } else {
-                    var status = {
-                        isVisible: true,
-                        text: j,
-                        isHoliday: false
-                    };
-
                     var theDay = new Date(this.year, this.month, j).getTime();
 
-                    status.isOutOfRange = theDay < this.minDay || theDay > this.maxDay;
-                    status.isFull = fullDaysOfMonth && fullDaysOfMonth.length > 0 && $.inArray(j, fullDaysOfMonth) > -1;
-                    status.isChecked = selectedDaysOfMonth && selectedDaysOfMonth.length > 0 && $.inArray(j, selectedDaysOfMonth) > -1;
-                    status.isOrdered = orderedDaysOfMonth && orderedDaysOfMonth.length > 0 && $.inArray(j, orderedDaysOfMonth) > -1;
-
-                    tmp.push(status);
+                    tmp.push({
+                        tdClass: theDay < this.minDay || theDay > this.maxDay ? "out-of-range" : "",
+                        text: j,
+                        isFull: fullDaysOfMonth && fullDaysOfMonth.length > 0 && $.inArray(j, fullDaysOfMonth) > -1,
+                        isOrdered: orderedDaysOfMonth && orderedDaysOfMonth.length > 0 && $.inArray(j, orderedDaysOfMonth) > -1,
+                        labelClass: selectedDaysOfMonth && selectedDaysOfMonth.length > 0 && $.inArray(j, selectedDaysOfMonth) > -1 ? "the-label option-checked" : "the-label"
+                    });
                 }
             }
 
             while (tmp.length < 7) {
                 tmp.push({
-                    isVisible: false
+                    tdClass: "",
+                    text: "",
+                    isFull: false,
+                    isOrdered: false,
+                    labelClass: ""
                 });
             }
 
-            result.push(tmp);
+            result.push({
+                days: tmp
+            });
 
             return result;
-        },
+        }
+    },
+    methods: {
         showCalendar: function () {
-            $("#calendar").html(calendarBodyTemplate({
-                    dates: this.getDates(),
-                    canShowPreviousMonth: new Date(this.year, this.month, 1) > this.minDay,
-                    canShowNextMonth: new Date(this.year, this.month + 1, 0) < this.maxDay
-                }
-            ));
-
             $(".full-image").attr("src", fullImageUrl);
             $(".ordered-image").attr("src", orderedImageUrl);
 
-            rebind();
+            $(".the-label > input[type=checkbox]").change(function () {
+                var label = $(this).parent();
+
+                var yearMonth = vue.getYearMonth;
+
+                if (!vue.$data.selectedDates[yearMonth]) {
+                    vue.$data.selectedDates[yearMonth] = [];
+                }
+                var selectedDaysOfMonth = vue.$data.selectedDates[yearMonth];
+
+                var theDay = parseInt($(this).val());
+                if (this.checked) {
+                    label.addClass("checked");
+                    selectedDaysOfMonth.push(theDay);
+                } else {
+                    label.removeClass("checked");
+                    var index = $.inArray(theDay, selectedDaysOfMonth);
+                    selectedDaysOfMonth.splice(index, 1);
+                }
+                console.log(vue.getSelectedDates);
+            });
+
+            console.log(vue.getSelectedDates);
         },
         showPreviousMonth: function () {
             var nextMonth = new Date(this.year, this.month - 1, 1);
@@ -145,37 +174,6 @@ var vueModel = {
         }
     }
 };
-
-function rebind() {
-    var tmp = vueModel;
-    vue.$destroy();
-    vueModel = tmp;
-    vue = new Vue(vueModel);
-
-    $(".the-label > input[type=checkbox]").change(function () {
-        var label = $(this).parent();
-
-        var yearMonth = vue.getYearMonth;
-
-        if (!vue.$data.selectedDates[yearMonth]) {
-            vue.$data.selectedDates[yearMonth] = [];
-        }
-        var selectedDaysOfMonth = vue.$data.selectedDates[yearMonth];
-
-        var theDay = parseInt($(this).val());
-        if (this.checked) {
-            label.addClass("checked");
-            selectedDaysOfMonth.push(theDay);
-        } else {
-            label.removeClass("checked");
-            var index = $.inArray(theDay, selectedDaysOfMonth);
-            selectedDaysOfMonth.splice(index, 1);
-        }
-        console.log(vue.getSelectedDates);
-    });
-
-    console.log(vue.getSelectedDates);
-}
 
 $(document).ready(function () {
     vue = new Vue(vueModel);
