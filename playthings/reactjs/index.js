9964a5275a65295ec4aa1a236490218819a82a41
yaoyao <yaoyao12306@163.com>
2015-08-12 08:53:09 +0800

    save

--- playthings/reactjs/index.js
+++ playthings/reactjs/index.js
@@ -1,12 +1,22 @@
-var names = ['a', 'b', 'c'];
+var TestComponent = React.createClass({displayName: "TestComponent",
+    getInitialState: function () {
+        return {
+            names: ['a', 'b', 'c']
+        }
+    },
+    render: function () {
+        return React.createElement("div", null, 
+            
+                this.state.names.map(function (name) {
+                    return React.createElement("div", {style: {color:"red"}, className: "test"}, 
+                        "Hello, ", name, "!")
+                })
+            
+        )
+    }
+});
 
 React.render(
-    React.createElement("div", null, 
-        
-            names.map(function (name) {
-                return React.createElement("div", {style: {color:"red"}, className: "test"}, "Hello, ", name, "!")
-            })
-        
-    ),
+    React.createElement(TestComponent, null),
     document.getElementById("container")
 );
--- playthings/reactjs/index.jsx
+++ playthings/reactjs/index.jsx
@@ -1,12 +1,22 @@
-var names = ['a', 'b', 'c'];
+var TestComponent = React.createClass({
+    getInitialState: function () {
+        return {
+            names: ['a', 'b', 'c']
+        }
+    },
+    render: function () {
+        return <div>
+            {
+                this.state.names.map(function (name) {
+                    return <div style={{color:"red"}} className="test">
+                        Hello, {name}!</div>
+                })
+            }
+        </div>
+    }
+});
 
 React.render(
-    <div>
-        {
-            names.map(function (name) {
-                return <div style={{color:"red"}} className="test">Hello, {name}!</div>
-            })
-        }
-    </div>,
+    <TestComponent/>,
     document.getElementById("container")
 );
